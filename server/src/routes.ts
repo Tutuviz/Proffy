7677e8717875878138995c87d446bb72fd76118b
Sara Passos <arthur.opassos@gmail.com>
2020-08-05 18:48:27 -0300

    fix: malfunction

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import ClassesController from "./controllers/classesController";
+import ClassesController from "./controllers/ClassesController";
 import ConnectionsController from "./controllers/ConnectionsController";
 
 const routes = express.Router();
