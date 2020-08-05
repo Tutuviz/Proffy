c06abeea72eb6fa6ae0f399e9cbc1e106396db36
Sara Passos <arthur.opassos@gmail.com>
2020-08-05 18:46:42 -0300

    feat: responsive total connections

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import api from "../../services/api";
+
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import landingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import logoImg from "../../assets/images/logo.svg";
 
 import "./style.css";
 
-import logoImg from "../../assets/images/logo.svg";
-import landingImg from "../../assets/images/landing.svg";
-
-import studyIcon from "../../assets/images/icons/study.svg";
-import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
-import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
-
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("/connections").then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -34,7 +45,7 @@ function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexoes já realizadas
+          Total de {totalConnections} conexoes já realizadas
           <img src={purpleHeartIcon} alt="Coracao Roxo" />
         </span>
       </div>
